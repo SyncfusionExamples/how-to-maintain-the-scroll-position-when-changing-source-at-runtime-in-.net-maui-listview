2f60875b900a04682f2835d7a09144f0b92c30c8
riyas <riyashameed.m@syncfusion.com>
2023-11-09 17:33:45 +0530

    Modified

--- ListViewMaui/Platforms/MacCatalyst/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace ListViewMaui;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
